ba34337b07612377b2c9ab67cb51293eeb2d8814
Rongjian Zhang <pd4d10@gmail.com>
2022-10-01 13:30:34 +0800

    chore: split schema update script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# https://docs.github.com/en/graphql/overview/public-schema
-curl -o lib/gql_github/schema.graphql https://docs.github.com/public/schema.docs.graphql
-
 flutter pub run build_runner watch --delete-conflicting-outputs
---
+++ scripts/update-schema.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# https://docs.github.com/en/graphql/overview/public-schema
+curl -o lib/gql_github/schema.graphql https://docs.github.com/public/schema.docs.graphql
